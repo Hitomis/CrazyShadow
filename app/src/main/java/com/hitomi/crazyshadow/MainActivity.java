c5a0d1133cf1fe3d966bf92e9af0dd86af300335
hitomi <1670957065@qq.com>
2016-10-25 17:43:40 +0800

    Demo

--- app/src/main/java/com/hitomi/crazyshadow/MainActivity.java
+++ app/src/main/java/com/hitomi/crazyshadow/MainActivity.java
@@ -5,11 +5,32 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.hitomi.cslibrary.CrazyShadow;
 import com.hitomi.cslibrary.base.CrazyShadowDirection;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View titleView;
+
+    private View drawView0, drawView1;
+
+    private View fallView0, fallView1;
+
+    private View wrapView0, wrapView1;
+
+    private View shadowView0, shadowView1;
+
+    private CrazyShadow titleCrazyShadow;
+
+    private CrazyShadow drawCrazyShadow0, drawCrazyShadow1;
+
+    private CrazyShadow fallCrazyShadow0, fallCrazyShadow1;
+
+    private CrazyShadow wrapCrazyShadow0, wrapCrazyShadow1;
+
+    private CrazyShadow shadowCrazyShadow0, shadowCrazyShadow1;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
@@ -17,63 +38,113 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new CrazyShadow.Builder()
+        initView();
+        initShadow();
+        initListener();
+
+    }
+
+    private void initListener() {
+        titleView.setOnClickListener(this);
+        drawView0.setOnClickListener(this);
+        drawView1.setOnClickListener(this);
+        fallView0.setOnClickListener(this);
+        fallView1.setOnClickListener(this);
+        wrapView0.setOnClickListener(this);
+        wrapView1.setOnClickListener(this);
+        shadowView0.setOnClickListener(this);
+        shadowView1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.relay_title:
+                titleCrazyShadow.remove();
+                break;
+            case R.id.relay_draw0:
+                drawCrazyShadow0.remove();
+                break;
+            case R.id.relay_draw1:
+                drawCrazyShadow1.remove();
+                break;
+            case R.id.relay_fall0:
+                fallCrazyShadow0.remove();
+                break;
+            case R.id.relay_fall1:
+                fallCrazyShadow1.remove();
+                break;
+            case R.id.relay_wrap0:
+                wrapCrazyShadow0.remove();
+                break;
+            case R.id.relay_wrap1:
+                wrapCrazyShadow1.remove();
+                break;
+            case R.id.relay_shadow0:
+                shadowCrazyShadow0.remove();
+                break;
+            case R.id.relay_shadow1:
+                shadowCrazyShadow1.remove();
+                break;
+        }
+    }
+
+    private void initShadow() {
+        titleCrazyShadow = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.BOTTOM)
                 .setShadowRadius(dip2Px(5))
                 .setImpl(CrazyShadow.IMPL_WRAP)
-                .action(findViewById(R.id.relay_title));
+                .action(titleView);
 
-
-        /*********************************************************************************/
-        new CrazyShadow.Builder()
+        drawCrazyShadow0 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
                 .setBaseShadowColor(Color.RED)
                 .setImpl(CrazyShadow.IMPL_DRAW)
-                .action(findViewById(R.id.relay_draw0));
+                .action(drawView0);
 
-        new CrazyShadow.Builder()
+        drawCrazyShadow1 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
                 .setCorner(dip2Px(5))
                 .setBackground(Color.parseColor("#96a993"))
                 .setImpl(CrazyShadow.IMPL_DRAW)
-                .action(findViewById(R.id.relay_draw1));
+                .action(drawView1);
 
-        new CrazyShadow.Builder()
+        fallCrazyShadow0 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
                 .setImpl(CrazyShadow.IMPL_FALL)
-                .action(findViewById(R.id.relay_fall0));
+                .action(fallView0);
 
-        new CrazyShadow.Builder()
+        fallCrazyShadow1 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
                 .setCorner(dip2Px(4))
                 .setImpl(CrazyShadow.IMPL_FALL)
-                .action(findViewById(R.id.relay_fall1));
+                .action(fallView1);
 
-        new CrazyShadow.Builder()
+        wrapCrazyShadow0 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
                 .setImpl(CrazyShadow.IMPL_WRAP)
-                .action(findViewById(R.id.relay_wrap0));
+                .action(wrapView0);
 
-        new CrazyShadow.Builder()
+        wrapCrazyShadow1 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(5))
                 .setCorner(dip2Px(8))
                 .setImpl(CrazyShadow.IMPL_WRAP)
-                .action(findViewById(R.id.relay_wrap1));
+                .action(wrapView1);
 
-        new CrazyShadow.Builder()
+        shadowCrazyShadow0 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.ALL)
                 .setShadowRadius(dip2Px(3))
@@ -82,12 +153,24 @@ public class MainActivity extends AppCompatActivity {
                 .setImpl(CrazyShadow.IMPL_WRAP)
                 .action(findViewById(R.id.relay_shadow0));
 
-        new CrazyShadow.Builder()
+        shadowCrazyShadow1 = new CrazyShadow.Builder()
                 .setContext(this)
                 .setDirection(CrazyShadowDirection.TOP)
                 .setShadowRadius(dip2Px(5))
                 .setImpl(CrazyShadow.IMPL_WRAP)
-                .action(findViewById(R.id.relay_shadow));
+                .action(findViewById(R.id.relay_shadow1));
+    }
+
+    private void initView() {
+        titleView = findViewById(R.id.relay_title);
+        drawView0 = findViewById(R.id.relay_draw0);
+        drawView1 = findViewById(R.id.relay_draw1);
+        fallView0 = findViewById(R.id.relay_fall0);
+        fallView1 = findViewById(R.id.relay_fall1);
+        wrapView0 = findViewById(R.id.relay_wrap0);
+        wrapView1 = findViewById(R.id.relay_wrap1);
+        shadowView0 = findViewById(R.id.relay_shadow0);
+        shadowView1 = findViewById(R.id.relay_shadow1);
     }
 
 
